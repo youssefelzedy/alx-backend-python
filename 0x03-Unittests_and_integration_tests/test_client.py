a1cebeb5ac80d9afe81bd592149f4bc3ebb2c078
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-01 00:26:33 +0300

    8 task

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -3,9 +3,12 @@
 """TestGithubOrgClient"""
 
 import unittest
-from parameterized import parameterized
+import json
+from parameterized import parameterized, parameterized_class
+from unittest import mock
+from unittest.mock import patch, Mock, PropertyMock
 from client import GithubOrgClient
-from unittest.mock import patch, Mock
+from fixtures import TEST_PAYLOAD
 
 
 class TestGithubOrgClient(unittest.TestCase):
@@ -66,26 +69,26 @@ class TestGithubOrgClient(unittest.TestCase):
         self.assertEqual(result, expectation)
 
 
-@parameterized_class(['org_payload', 'repos_payload',
-                      'expected_repos', 'apache2_repos'], TEST_PAYLOAD)
-class TestIntegrationGithubOrgClient(unittest.TestCase):
-    """Integration test"""
-    @classmethod
-    def setUpClass(cls):
-        cls.get_patcher = patch('requests.get', side_effect=[
-            cls.org_payload, cls.repos_payload
-        ])
-        cls.mocked_get = cls.get_patcher.start()
+    @parameterized_class(['org_payload', 'repos_payload',
+                        'expected_repos', 'apache2_repos'], TEST_PAYLOAD)
+    class TestIntegrationGithubOrgClient(unittest.TestCase):
+        """Integration test"""
+        @classmethod
+        def setUpClass(cls):
+            cls.get_patcher = patch('requests.get', side_effect=[
+                cls.org_payload, cls.repos_payload
+            ])
+            cls.mocked_get = cls.get_patcher.start()
 
-    @classmethod
-    def tearDownClass(cls):
-        cls.get_patcher.stop()
+        @classmethod
+        def tearDownClass(cls):
+            cls.get_patcher.stop()
 
-    def test_public_repos(self):
-        """test public repos """
+        def test_public_repos(self):
+            """test public repos """
 
-    def test_public_repos_with_license(self):
-        """test public with license"""
+        def test_public_repos_with_license(self):
+            """test public with license"""
 
 
 if __name__ == '__main__':

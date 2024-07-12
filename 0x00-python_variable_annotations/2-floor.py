2e8b17b3e4cfd5a2d76e86ee35b572fed51861a1
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-12 10:53:54 +0300

    final task

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+''' Floor a float and return the result '''
+
+
+def floor(n: float) -> int:
+    ''' Function that takes a float and returns its floor '''
+    return int(n)

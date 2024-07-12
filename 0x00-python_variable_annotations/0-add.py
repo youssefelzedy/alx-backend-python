6683af15faee607b3b7914c2e5d84c9d94802a1a
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-12 10:51:01 +0300

    final task

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+''' Add two floats and return the result '''
+
+
+def add(a: float, b: float) -> float:
+    ''' Function that takes two floats and returns their sum '''
+    return a + b

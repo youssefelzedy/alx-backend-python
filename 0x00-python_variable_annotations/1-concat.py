d6fa01f9e4a6a49f33deef87088c67fe9a5e5487
Youssef Elzedy <youssefelzedy90@gmail.com>
2024-07-12 10:52:28 +0300

    final task

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+''' Concatenate two strings and return the result '''
+
+
+def concat(str1: str, str2: str) -> str:
+    ''' Function that takes two strings and returns their concatenation'''
+    return str1 + str2
